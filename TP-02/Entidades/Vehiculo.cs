4decf8e4868284bc2522a9b1c9df4dad7a66f759
DiacuR <47385825+DiacuR@users.noreply.github.com>
2020-05-10 22:49:51 -0300

    Resubido

---
+++ TP-02/Entidades/Vehiculo.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// La clase Vehiculo no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Vehiculo
+    {
+        #region Enumerados
+        /// <summary>
+        /// Enumerado con los tipos de marca
+        /// </summary>
+        public enum EMarca
+        {
+            Chevrolet, Ford, Renault, Toyota, BMW, Honda
+        }
+        /// <summary>
+        /// Enumerado con los diferentes tamaños del Vehiculo
+        /// </summary>
+        public enum ETamanio
+        {
+            Chico, Mediano, Grande
+        }
+        #endregion
+
+        EMarca marca;
+        string chasis;
+        ConsoleColor color;
+
+        #region Constructor
+        /// <summary>
+        /// Constructor de Vehiculo
+        /// </summary>
+        /// <param name="chasis"></param>
+        /// <param name="marca"></param>
+        /// <param name="color"></param>
+        public Vehiculo(string chasis, EMarca marca, ConsoleColor color)
+        {
+            this.chasis = chasis;
+            this.marca = marca;
+            this.color = color;
+        }
+        #endregion
+
+        #region Propiedad
+        /// <summary>
+        /// ReadOnly: Retornará el tamaño
+        /// </summary>
+        protected abstract ETamanio Tamanio { get; }
+        #endregion
+
+        #region Metodos
+        /// <summary>
+        /// Publica todos los datos del Vehiculo.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+        #endregion
+
+        #region Operadores
+        /// <summary>
+        /// Sobreescribe el convertidor explicito para mostrar los datos del objeto Vehiculo o Heredados.
+        /// </summary>
+        /// <param name="p"></param>
+        public static explicit operator string(Vehiculo p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CHASIS: "+ p.chasis);
+            sb.AppendLine("MARCA : "+ p.marca.ToString());
+            sb.AppendLine("COLOR : "+ p.color.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Dos vehiculos son iguales si comparten el mismo chasis
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vehiculo v1, Vehiculo v2)
+        {
+            return (v1.chasis == v2.chasis);
+        }
+        /// <summary>
+        /// Dos vehiculos son distintos si su chasis es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vehiculo v1, Vehiculo v2)
+        {
+            return !(v1 == v2);
+        }
+        #endregion
+    }
+}
